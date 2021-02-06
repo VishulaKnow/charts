38b84d7b0cf33a75dc690e1515d63d4401ab6642
Valentin <valya.shulaev@gmail.com>
2021-02-06 16:37:15 +0300

    Tests init

---
+++ src/__tests__/engine/helperTest.ts
@@ -0,0 +1,8 @@
+import { Helper } from "../../engine/helper";
+
+test('Should return tuple of two numbers which equal transaleX and translateY', () => {
+    expect(Helper.getTranslateNumbers('translate(14, 34)')).toEqual([14, 34]);
+    expect(Helper.getTranslateNumbers('translate(2000, 0)')).toEqual([2000, 0]);
+    expect(Helper.getTranslateNumbers('translate(-12, -123)')).toEqual([-12, -123]);
+    expect(Helper.getTranslateNumbers('translate(0, 0)')).toEqual([0, 0]);
+});
