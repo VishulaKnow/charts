a1e5a40e918b48aa3d4fdc89df8e9666909322a2
Valentin <valya.shulaev@gmail.com>
2021-02-15 17:01:51 +0300

    Append updateData method to engine

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -1,25 +1,26 @@
 import { Block } from './block/block';
 import { ValueFormatter } from './valueFormatter';
 import { ChartRenderer } from './chartRenderer';
-import { Model } from '../model/model';
+import { DataSource, Model } from '../model/model';
 
 export default class Engine {
     private block: Block;
 
-    public render(model: Model, data: any, parentElement: HTMLElement): void {       
-        ValueFormatter.setFormatFunction(model.dataFormat.formatters);
+    public render(model: Model, data: DataSource, parentElement: HTMLElement): void {       
         this.block = new Block(model.blockCanvas.cssClass, parentElement);
+
         this.block.renderWrapper(model.blockCanvas.size);
-        
-        if(model.options.type === '2d')
-            ChartRenderer.render2D(this.block, model, data);
-        else if(model.options.type === 'polar')
-            ChartRenderer.renderPolar(this.block, model, data);
-        else if(model.options.type === 'interval')
-            ChartRenderer.renderInterval(this.block, model, data);
+        ValueFormatter.setFormatFunction(model.dataFormat.formatters);
+
+        this.renderCharts(model, data);
     }
 
-    public updateFullBlock(model: Model, data: any): void {
+    public updateData(newModel: Model, newData: DataSource): void {
+        this.block.getSvg().remove();
+        this.renderCharts(newModel, newData);
+    }
+
+    public updateFullBlock(model: Model, data: DataSource): void {
         this.destroy();
         this.render(model, data, this.block.parentElement);
     }
@@ -30,5 +31,14 @@ export default class Engine {
 
     public destroy(): void {
         this.block.getWrapper().remove();
+    }
+
+    private renderCharts(model: Model, data: DataSource): void {
+        if(model.options.type === '2d')
+            ChartRenderer.render2D(this.block, model, data);
+        else if(model.options.type === 'polar')
+            ChartRenderer.renderPolar(this.block, model, data);
+        else if(model.options.type === 'interval')
+            ChartRenderer.renderInterval(this.block, model, data);
     }
 }
