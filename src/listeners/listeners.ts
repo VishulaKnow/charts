7ed3567995bf36b60e6d8e78bc25369ac9788d29
Valentin Shulaev <valya.shulaev@gmail.com>
2021-03-10 12:46:04 +0300

    update listeners

--- src/listeners/listeners.ts
+++ src/listeners/listeners.ts
@@ -638,13 +638,15 @@ const model2 = assembleModel(config2, data, designerConfig);
 const engine2 = new Engine(4);
 engine2.render(model2, getPreparedData(model2, data, config2), document.querySelector('.main-wrapper2'));
 
-import { interval } from 'd3-timer'
+setInterval(() => {
+    const newData = ListenersHelper.getCopy(data);
+    newData['dataSet'].push({
+        brand: 'newBrand',
+        price: 130
+    });
+    newData["dataSet"][ListenersHelper.randInt(0, 8)]['price'] = ListenersHelper.randInt(0, 100);
+    newData["dataSet"][ListenersHelper.randInt(0, 8)]['count'] = ListenersHelper.randInt(0, 100);
 
-// setInterval(() => {
-//     const newData = ListenersHelper.getCopy(data);
-//     newData["dataSet"][ListenersHelper.randInt(0, 8)]['price'] = ListenersHelper.randInt(0, 100);
-//     newData["dataSet"][ListenersHelper.randInt(0, 8)]['count'] = ListenersHelper.randInt(0, 100);
-
-//     const newModel = getUpdatedModel(config, newData, designerConfig);
-//     engine.updateValues(newModel, getPreparedData(newModel, newData, config));
-// }, 2000);
+    const newModel = getUpdatedModel(config, newData, designerConfig);
+    engine.updateValues(newModel, getPreparedData(newModel, newData, config));
+}, 2000);
