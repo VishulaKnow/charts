bc249d9d848fba25031cdc98536f0c69b3c548bd
Valentin Shulaev <valya.shulaev@gmail.com>
2021-03-17 09:28:01 +0300

    split key index getting

--- src/engine/features/tolltip/tooltipHelper.ts
+++ src/engine/features/tolltip/tooltipHelper.ts
@@ -138,32 +138,15 @@ export class TooltipHelper {
         coordinate[1] - TOOLTIP_ARROW_PADDING_Y - tooltipBlockNode.getBoundingClientRect().height - verticalPad];
     }
 
-    public static getKeyIndex(pointer: [number, number], orient: ChartOrientation, margin: BlockMargin, blockSize: Size, scaleKey: AxisScale<any>, scaleKeyType: ScaleKeyType): number {
-        const pointerAxisType = orient === 'vertical' ? 0 : 1; // 0 - координата поинтера по оси x, 1 - по оси y
-        const marginByOrient = orient === 'vertical' ? margin.left : margin.top;
+    public static getKeyIndex(pointer: [number, number], chartOrient: ChartOrientation, margin: BlockMargin, blockSize: Size, scaleKey: AxisScale<any>, scaleKeyType: ScaleKeyType): number {
+        const pointerAxisType = chartOrient === 'vertical' ? 0 : 1; // 0 - координата поинтера по оси x, 1 - по оси y
+        const marginByOrient = chartOrient === 'vertical' ? margin.left : margin.top;
         const scaleStep = Scale.getScaleStep(scaleKey);
 
         if (scaleKeyType === 'point') {
-            const point = pointer[pointerAxisType] - marginByOrient + scaleStep / 2;
-            if (point < 0)
-                return 0;
-
-            return Math.floor(point / scaleStep);
+            return this.getKeyIndexOfPoint(pointer, scaleStep, marginByOrient, pointerAxisType);
         } else {
-            const chartBlockSizeByDir = orient === 'vertical'
-                ? blockSize.width - margin.left - margin.right
-                : blockSize.height - margin.top - margin.bottom;
-
-            const outerPadding = chartBlockSizeByDir - scaleStep * scaleKey.domain().length;
-
-            if (pointer[pointerAxisType] - marginByOrient - 1 < outerPadding / 2)
-                return 0; // Самый первый элемент
-            if (pointer[pointerAxisType] - marginByOrient - 1 + outerPadding / 2 > chartBlockSizeByDir)
-                return scaleKey.domain().length - 1; // последний индекс
-
-            const point = pointer[pointerAxisType] - outerPadding / 2 - marginByOrient - 1;
-
-            return Math.floor(point / scaleStep);
+            return this.getKeyIndexOfBand(pointer, scaleStep, marginByOrient, pointerAxisType, blockSize, margin, chartOrient, scaleKey);
         }
     }
 
@@ -196,6 +179,31 @@ export class TooltipHelper {
         }
 
         return attributes;
+    }
+
+    private static getKeyIndexOfPoint(pointer: [number, number], scaleStep: number, marginByOrient: number, pointerAxisType: 0 | 1): number {
+        const point = pointer[pointerAxisType] - marginByOrient + scaleStep / 2;
+        if (point < 0)
+            return 0;
+
+        return Math.floor(point / scaleStep);
+    }
+
+    private static getKeyIndexOfBand(pointer: [number, number], scaleStep: number, marginByOrient: number, pointerAxisType: 0 | 1, blockSize: Size, margin: BlockMargin, chartOrient: ChartOrientation, scaleKey: AxisScale<any>): number {
+        const chartBlockSizeByDir = chartOrient === 'vertical'
+            ? blockSize.width - margin.left - margin.right
+            : blockSize.height - margin.top - margin.bottom;
+
+        const outerPadding = chartBlockSizeByDir - scaleStep * scaleKey.domain().length;
+
+        if (pointer[pointerAxisType] - marginByOrient - 1 < outerPadding / 2)
+            return 0; // Самый первый элемент
+        if (pointer[pointerAxisType] - marginByOrient - 1 + outerPadding / 2 > chartBlockSizeByDir)
+            return scaleKey.domain().length - 1; // последний индекс
+
+        const point = pointer[pointerAxisType] - outerPadding / 2 - marginByOrient - 1;
+
+        return Math.floor(point / scaleStep);
     }
 
     private static fillTooltipContent(tooltipContentBlock: Selection<BaseType, unknown, BaseType, unknown>, markColor: string, tooltipText: string): void {
