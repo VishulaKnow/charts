815e67f176b6a3944f5ef43b2cd4e7aa09f35185
Valentin Shulaev <valya.shulaev@gmail.com>
2021-03-22 12:40:27 +0300

    gridLine refactor

--- src/engine/twoDimensionalNotation/twoDimensionalManager.ts
+++ src/engine/twoDimensionalNotation/twoDimensionalManager.ts
@@ -77,7 +77,7 @@ export class TwoDimensionalManager {
 
         Axis.update(block, scales, options.scale, options.axis, model.blockCanvas.size, keyDomainEquality);
 
-        GridLine.rerender(block,
+        GridLine.update(block,
             options.additionalElements.gridLine.flag,
             options.axis.keyAxis,
             options.axis.valueAxis,
