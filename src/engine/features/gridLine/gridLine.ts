d61be7d1735f63346743a6b1d6e05cbdf9beafce
Valentin Shulaev <valya.shulaev@gmail.com>
2024-11-11 09:59:39 +0300

    remove unnecessary code

--- src/engine/features/gridLine/gridLine.ts
+++ src/engine/features/gridLine/gridLine.ts
@@ -21,8 +21,6 @@ export class GridLine {
             const lineAttributes = GridLineHelper.getKeyLineAttributes(axes.key, scales.value);
             this.renderLine(block, axes.key, lineAttributes, options);
         }
-
-        this.removeGridLinesOnAxes(block, axes.key, axes.value, true);
     }
 
     public static update(block: Block, options: GridLineOptions, axes: IAxisModel, blockSize: Size, margin: BlockMargin, scales: Scales): void {
@@ -58,34 +56,6 @@ export class GridLine {
             .selectAll(`.${valueAxisClass}`)
             .selectAll('g.tick')
             .selectAll(`.${this.gridLineClass}`)
-            .remove();
-    }
-
-    private static removeGridLinesOnAxes(block: Block, keyAxis: AxisModelOptions, valueAxis: AxisModelOptions, excludeKey: boolean): void {
-        let tickOnKeyAxisSelector = '';
-        let tickOnValueAxisSelector = '';
-
-        if (valueAxis.orient === 'right' || valueAxis.orient === 'bottom')
-            tickOnValueAxisSelector = ':last-of-type';
-        if (keyAxis.orient === 'bottom' || keyAxis.orient === 'right')
-            tickOnKeyAxisSelector = ':last-of-type';
-
-        const tickOnKey = block.getSvg()
-            .select(`.${valueAxis.cssClass}`)
-            .select(`g.tick${tickOnKeyAxisSelector}`);
-        const tickOnValue = block.getSvg()
-            .select(`.${keyAxis.cssClass}`)
-            .select(`g.tick${tickOnValueAxisSelector}`)
-
-
-        // this.rmGridLineOnTick(tickOnKey);
-        // if (!excludeKey)
-        //     this.rmGridLineOnTick(tickOnValue);
-    }
-
-    private static rmGridLineOnTick(tick: Selection<BaseType, unknown, HTMLElement, any>) {
-        tick
-            .select(`.${this.gridLineClass}`)
             .remove();
     }
 }
